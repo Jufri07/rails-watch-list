1dde3cf35420bbac600c8f7426282e66323cae6a
Jufri Ali <faqat0_0@hotmail.com>
2023-02-18 15:58:18 +0800

    railswatch partial

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,26 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get '/lists/new', to: 'lists#new'
+  get '/lists/index', to: 'lists#index'
+  get '/lists/show', to: 'lists#show'
+  get '/lists/create', to: 'lists#create'
+
+
+  resources :lists do
+    resources :bookmarks
+  end
+
+  get 'bookmarks/new', to: 'bookmarks#new'
+  get 'bookmarks/show', to: 'bookmarks#show'
+  delete '/bookmarks/:id', to: 'bookmarks#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
